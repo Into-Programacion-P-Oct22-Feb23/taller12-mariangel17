079681fb281129d922ab25ea1cbd8e4a6e7fbd00
mariangel17 <115736474+mariangel17@users.noreply.github.com>
2023-01-20 11:31:55 -0500

    Problema1

--- Ejercicios/src/ejercicios/Problema00.java
+++ Ejercicios/src/ejercicios/Problema00.java
@@ -15,7 +15,34 @@ public class Problema00 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double[][] datos = {{10, 2}, {1, 5}, {6, 2}, {7, 8}};
+        double[][] resultado = new double[4][2];
+
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                resultado[i][j] = factorial(datos[i][j]);
+            }
+        }
+
+        for (int i = 0; i < resultado.length; i++) {
+            for (int j = 0; j < resultado[i].length; j++) {
+                System.out.print(resultado[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 
+     * @param numero es el numero que se va a calcular el factorial
+     * @return el factorial del numero
+     */
+    public static double factorial(double numero) {
+        if (numero == 0) {
+            return 1;
+        } else {
+            return numero * factorial(numero - 1);
+        }
     }
     
 }
